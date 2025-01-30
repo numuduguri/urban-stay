687174e3373f3356ad31dc1fa962b737bdf7263f
numuduguri <raju.techm1990@.com>
2025-01-31 00:08:12 +0530

    added project

---
+++ urban-stay/src/main/java/com/urbanstay/UrbanStayApplication.java
@@ -0,0 +1,13 @@
+package com.urbanstay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanStayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanStayApplication.class, args);
+	}
+
+}
---
+++ urban-stay/src/test/java/com/urbanstay/UrbanStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbanstay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
